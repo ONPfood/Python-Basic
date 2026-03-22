b75b34734fcb9d213f47cbe5564200ab4daa45dd
AI-study <onpfood@gmail.com>
2026-03-21 21:55:27 -0700

    함수(function)를 공부하자

---
+++ 01 Basic/function.py
@@ -0,0 +1,15 @@
+# function
+# 
+
+
+def calc(ddd):
+    if ddd < 10:
+        return "FREE"
+    else:
+        return ddd * 10
+
+# call function
+print(5, "is", calc(5), ".")
+print(15, "is", calc(15), ".")
+
+        
